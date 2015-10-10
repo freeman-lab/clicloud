a15e396de0ba3808b806044f9a8362f2439ca5af
freeman <the.freeman.lab@gmail.com>
2015-10-09 21:17:01 -0400

    Minor refactor

--- index.js
+++ index.js
@@ -6,32 +6,30 @@ var chalk = require('chalk')
 var options = require('./lib/options.js')
 var commands = require('./lib/commands.js')
 
-module.exports.parse = function(argv, extra) {
-
+function CLI(extra) {
   if (extra && extra.options) options = options.concat(extra.options)
+  if (extra && extra.commands) commands = commands.concat(extra.commands)
+  this.options = cliclopts(options)
+  this.commands = commands
+  return this
+}
 
-  var cliopts = cliclopts(options)
-  var args = minimist(argv.slice(2), cliopts.options())
+CLI.prototype.parse = function(argsin) {
+  var args = minimist(argsin.slice(2), this.options.options())
   args.action = args._[0]
   args.cluster = args._[1]
   args.tag = args._[2]
   args.id = args._[3]
   return args
-
 }
 
-module.exports.init = function(args, cloud, extra) {
-
-  if (extra && extra.options) options = options.concat(extra.options)
-  if (extra && extra.commands) commands = commands.concat(extra.commands)
-  var cliopts = cliclopts(options)
-
+CLI.prototype.init = function(args, cloud) {
   var actions = commands.map( function(action) {return action.name})
   var prefix = chalk.gray('[' + args.action + '] ')
 
   if (args.help || !args.action || !args.cluster) {
     console.log('Usage: tinycloud <action> <cluster> [options]')
-    cliopts.print()
+    this.options.print()
     process.exit()
   }
 
@@ -54,5 +52,6 @@ module.exports.init = function(args, cloud, extra) {
 
   var route = subcommand(config)
   route([args.action, args, cloud])
-
 }
+
+module.exports = CLI
